6c169668bb79f03d8a316a2d713b77618697134e
capt-alien <eric.botcher@presidio.edu>
2019-01-31 14:05:46 -0800

    Hello world

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-check cehck ckechek
+// requirements
+const express = require('express')
+const app = express()
+
+// midleware
+app.get('/',(req,res) =>
+    res.send('What up Bitches!!?')
+})
+
+app.listen(3000, () =>{
+    console.log('App listening on port 3000!')
+})
