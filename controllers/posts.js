a3264f3f252bda23bca82288b65ec64aa362f4c7
capt-alien <eric.botcher@presidio.edu>
2019-02-08 13:27:50 -0800

    tests implimented

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,6 @@ module.exports = (app) => {
   app.post('/posts/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
     const post = new Post(req.body);
-
     // SAVE INSTANCE OF POST MODEL TO DB
     post.save((err, post) => {
       // REDIRECT TO THE ROOT
@@ -30,7 +29,16 @@ module.exports = (app) => {
     })
   });
 
-
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id)
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   })
 
--- server.js
+++ server.js
@@ -27,3 +27,5 @@ const Post = require('./models/post');
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+module.exports = app;
